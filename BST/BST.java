740e555e6b76bc8728fddab6f71d3a49816e05f8
adgad2 <qetqe2@gmail.com>
2019-01-31 10:26:35 +0800

    树的前序遍历，完成

---
+++ BST/BST.java
@@ -0,0 +1,126 @@
+package BST;
+
+public class BST<E extends Comparable<E>> {
+
+     private class Node{
+        public E e;
+        public Node left,right;
+
+        public Node(E e){
+            this.e=e;
+            left=null;
+            right=null;
+        }
+    }
+
+    private Node root;
+    private  int size;
+
+    public BST(){
+        root=null;
+        size=0;
+    }
+
+    public int getSize(){
+        return  size;
+    }
+
+    public boolean isEmpty(){
+        return  size==0;
+    }
+
+    public void add(E e){
+        root=add(root,e);
+    }
+
+    //以像node为根的二分搜索树中插入元素E，递归算法
+    //返回插入新节点后二分搜索树的根
+    private Node add(Node node,E e){
+//        if (e.equals(node.e)){
+//            return;
+//        }else if (e.compareTo(node.e)<0 && node.left==null){
+//            node.left=new Node(e);
+//            size++;
+//            return;
+//        }else if (e.compareTo(node.e)>0 && node.right==null){
+//            node.right=new Node(e);
+//            size++;
+//            return;
+//        }
+        if (node==null){
+            size++;
+            return new Node(e);
+        }
+
+        if (e.compareTo(node.e)<0){
+            node.left=add(node.left,e);
+        }else if (e.compareTo(node.e)>0){
+            node.right=add(node.right,e);
+        }
+        return node;
+    }
+
+    //查看二分搜索树中是否包含元素e
+    public boolean contains(E e){
+        return contains(root,e);
+    }
+
+    private boolean contains(Node node,E e){
+
+        if (root==null){
+            return false;
+        }
+
+        if (e.compareTo(node.e)==0){
+            return true;
+        }else if (e.compareTo(node.e)<0){
+            return contains(node.left,e);
+        }else {
+            return contains(node.right,e);
+        }
+    }
+
+    //二分搜索树的前序遍历
+    public void preOrder(){
+        preOrder(root);
+    }
+
+    //前序遍历以node为根的二分搜索树，递归算法
+    private void preOrder(Node node){
+        if (node==null){
+            return;
+        }
+        System.out.println(node.e);
+
+        preOrder(node.left);
+        preOrder(node.right);
+
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res=new StringBuilder();
+        generateBSTString(root,0,res);
+        return res.toString();
+    }
+
+    private void generateBSTString(Node node,int depth,StringBuilder res){
+        if (node==null){
+            res.append(generateDepthString(depth)+"null\n");
+            return;
+        }
+
+        res.append(generateDepthString(depth)+node.e+"\n");
+        generateBSTString(node.left,depth+1,res);
+        generateBSTString(node.right,depth+1,res);
+
+    }
+
+    private String generateDepthString(int depth){
+        StringBuilder res=new StringBuilder();
+        for (int i=0;i<depth;i++){
+            res.append("--");
+        }
+        return res.toString();
+    }
+}
